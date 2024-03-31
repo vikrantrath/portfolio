c850829ba0d10c2fb6d680d2324ce435d52f4a67
Vikrant Rath <vikrant@Vikrants-MacBook-Air.local>
2024-03-31 18:40:34 +0530

    Init

---
+++ src/lib/Nested/Nested.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let count = 0;
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<h1 class="child">Child</h1>
+<button on:click={increment}>{count}</button>
+
+<style>
+	@import './nested.scss';
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Nested from '$lib/Nested/Nested.svelte';
+	let name: string = 'Vikrant';
+</script>
+
+<h1>Changing content with {name.toUpperCase()}</h1>
+<Nested />
+
+<style lang="scss">
+	h1 {
+		font-weight: 200;
+	}
+</style>
